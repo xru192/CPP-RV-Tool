7b6cee1eb9602e39f3aebe58f65f5d5125ccd992
rx26 <rx26@cornell.edu>
2024-04-18 23:13:05 -0400

    Remove non-project files

--- CCMOP/examples/C/MF/MFaspect/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cmop -v -d . /home/xyc/CLionProjects/examples/C/MF/MF.mop
-rv-monitor -c -p MF.rvm
-clang++  -O3  -fPIC -c  *.cc
-clang -r -o libaspect.a *.o
-echo "The aspect library  have been generated."
